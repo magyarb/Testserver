10dcb566b509adea624a8091d9c3dee9c991db51
magyarb <magyarb94@gmail.com>
2015-05-10 21:36:56 +0200

    asd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,17 +14,28 @@ public class Main {
         String clientSentence;
         String capitalizedSentence;
 
-        ServerSocket welcomeSocket = new ServerSocket(6789);
+        ServerSocket welcomeSocket = new ServerSocket(80);
+        System.out.println("1. Listening on "+ welcomeSocket.getLocalSocketAddress());
 
         while(true){
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+            try {
+                Socket connectionSocket = welcomeSocket.accept();
+                System.out.println("2. Listening to" + connectionSocket.getInetAddress());
+                BufferedReader inFromClient =
+                        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                clientSentence = inFromClient.readLine();
+                System.out.println("Local: " + connectionSocket.getLocalAddress() + ":" + connectionSocket.getLocalPort() + ", " + connectionSocket.getLocalSocketAddress());
+                System.out.println("Remote: " + connectionSocket.getInetAddress() + ":" + connectionSocket.getPort() + ", " + connectionSocket.getRemoteSocketAddress());
+                System.out.println("Received: " + clientSentence);
+                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizedSentence);
+            }
+            catch (Exception ex)
+            {
+                System.out.println("ERROR: "+ ex.toString());
+            }
         }
     }
 }
